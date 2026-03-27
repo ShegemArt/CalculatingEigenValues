56c4dc7c157cbb19187122f0619a71639d60d2bc
Shegem <artemsheg0001@gmail.com>
2026-03-27 23:33:59 +0300

    Add submodule, add all fiches

---
+++ CalculatingEigenValues/CalculatingEigenValues.h
@@ -0,0 +1,196 @@
+#pragma once
+#include <cmath>
+#include <vector>
+#include "../External/Matrix/Matrix/Matrix.h"
+#include "../External/Matrix/Matrix/Vector.h"
+#include "../External/Matrix/Matrix/LinearSystemSolve.h"
+using namespace std;
+
+
+void powerMethod(double& eigenvalue, Vector& x, Matrix A, int n, double tol, int max_iter = 100000) {
+    Vector x_old(n);
+
+    int iter = 0;
+
+    do {
+        x_old = x;
+
+        Vector y = A * x;
+
+        double norm = InfVectorNorm(y);
+        if (norm < 1e-12) break;
+
+        x = (1 / norm) * y;
+
+        iter++;
+
+    } while (InfVectorNorm(x - x_old) > tol && iter < max_iter);
+
+    Vector Ax = A * x;
+    double num = 0.0;
+    double den = 0.0;
+
+    for (int i = 0; i < n; i++) {
+        num += x.numbersVector[i] * Ax.numbersVector[i];
+        den += x.numbersVector[i] * x.numbersVector[i];
+    }
+
+    eigenvalue = num / den;
+}
+
+
+void inversePowerMethod(double& sigma, Vector& x, Matrix A, int n, double tol, int max_iter = 100000) {
+    Matrix I = Matrix::MakeIdentityMatrix(n);
+
+    Vector x_old(n);
+
+    int iter = 0;
+
+    while (iter < max_iter) {
+        x_old = x;
+
+        Matrix B = A - sigma * I;
+
+        Vector y = PLU(B, x, n);
+
+        double norm = InfVectorNorm(y);
+        if (norm < 1e-12) break;
+
+        x = (1 / norm) * y;
+
+        Vector Ax = A * x;
+        double num = 0.0;
+        double den = 0.0;
+
+        for (int i = 0; i < n; i++) {
+            num += x.numbersVector[i] * Ax.numbersVector[i];
+            den += x.numbersVector[i] * x.numbersVector[i];
+        }
+
+        double new_sigma = num / den;
+
+        if (abs(new_sigma - sigma) < tol)
+            break;
+
+        sigma = new_sigma;
+        iter++;
+    }
+}
+
+
+Matrix toHessenberg(Matrix A, int n) {
+    Matrix H = A;
+
+    Matrix I = Matrix::MakeIdentityMatrix(n);
+
+    for (int i = 0; i < n - 2; i++) {
+        Vector v(n);;
+
+        double s = 0.0;
+        for (int j = i + 1; j < n; j++) {
+            s += H.numbersMatrix[j][i] * H.numbersMatrix[j][i];
+        }
+
+        if (s < 1e-12) continue;
+
+        double sign = (H.numbersMatrix[i + 1][i] >= 0) ? 1.0 : -1.0;
+        s = sqrt(s) * sign;
+
+        v.numbersVector[i + 1] = H.numbersMatrix[i + 1][i] - s;
+
+        for (int j = i + 2; j < n; j++) {
+            v.numbersVector[j] = H.numbersMatrix[j][i];
+        }
+
+        double norm = 0.0;
+        for (int j = 0; j < n; j++)
+            norm += v.numbersVector[j] * v.numbersVector[j];
+
+        norm = sqrt(norm);
+        if (norm < 1e-12) continue;
+
+        for (int j = 0; j < n; j++)
+            v.numbersVector[j] /= norm;
+
+        Matrix Hh = I - 2 * (v * v);
+        H = Hh * H * Hh;
+    }
+
+    return H;
+}
+
+
+void givensQR(Matrix& Q, Matrix& R, int n) {
+    Q = Matrix::MakeIdentityMatrix(n);
+
+    for (int i = 0; i < n - 1; i++) {
+        double a = R.numbersMatrix[i][i];
+        double b = R.numbersMatrix[i + 1][i];
+
+        if (abs(b) < 1e-12) continue;
+
+        double r = sqrt(a * a + b * b);
+        double c = a / r;
+        double s = -b / r;
+
+        Matrix G = Matrix::MakeIdentityMatrix(n);
+
+        G.numbersMatrix[i][i] = c;
+        G.numbersMatrix[i][i + 1] = -s;
+        G.numbersMatrix[i + 1][i] = s;
+        G.numbersMatrix[i + 1][i + 1] = c;
+
+        R = G * R;
+        Q = Q * G.Transpose();
+    }
+}
+
+
+double wilkinsonShift(Matrix& A, int m) {
+    double d = (A.numbersMatrix[m - 2][m - 2] - A.numbersMatrix[m - 1][m - 1]) / 2.0;
+    double b = A.numbersMatrix[m - 1][m - 2];
+
+    double sign = (d >= 0) ? 1.0 : -1.0;
+
+    return A.numbersMatrix[m - 1][m - 1] -
+        sign * b * b / (abs(d) + sqrt(d * d + b * b));
+}
+
+
+vector<double> QR_algorithm(Matrix A, int n, double tol, int max_iter = 100000) {
+    Matrix H = toHessenberg(A, n);
+    vector<double> eigenvalues;
+
+    int m = n;
+
+    while (m > 1) {
+        int iter = 0;
+
+        while (abs(H.numbersMatrix[m - 1][m - 2]) > tol && iter < max_iter) {
+
+            double mu = wilkinsonShift(H, m);
+
+            for (int i = 0; i < m; i++)
+                H.numbersMatrix[i][i] -= mu;
+
+            Matrix Q = Matrix::MakeIdentityMatrix(m);
+            Matrix R = H;
+
+            givensQR(Q, R, m);
+
+            H = R * Q;
+
+            for (int i = 0; i < m; i++)
+                H.numbersMatrix[i][i] += mu;
+
+            iter++;
+        }
+
+        eigenvalues.push_back(H.numbersMatrix[m - 1][m - 1]);
+        m--;
+    }
+
+    eigenvalues.push_back(H.numbersMatrix[0][0]);
+
+    return eigenvalues;
+}
---
+++ CalculatingEigenValues/Test.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale>
+#include "CalculatingEigenValues.h"
+
+using namespace std;
+
+int main() {
+    double tol = 1e-8;
+    setlocale(LC_ALL, "Russian");
+
+    while (true) {
+        int n;
+        cout << "Введите размерность матрицы (0 - выход): ";
+        cin >> n;
+
+        if (n == 0) break;
+
+        Matrix A(n);
+        cout << "Введите матрицу:\n";
+        A.InputFromConsole();
+
+        while (true) {
+            cout << "\nВыберите метод:\n";
+            cout << "1 - Степенной метод\n";
+            cout << "2 - Обратный степенной метод\n";
+            cout << "3 - QR алгоритм (все собственные значения)\n";
+            cout << "4 - Ввести новую матрицу\n";
+            cout << "0 - Выход\n";
+
+            int choice;
+            cin >> choice;
+
+            if (choice == 0) return 0;   
+
+            if (choice == 4) break;
+
+            if (choice == 1) {
+                Vector x(n);
+                vector<double> init(n, 1.0);
+                x.numbersVector = init;
+
+                double eigen = 0.0;
+
+                powerMethod(eigen, x, A, n, tol);
+
+                cout << "Максимальное eigenvalue: " << eigen << endl;
+                cout << "Собственный вектор:\n";
+                double norma = L2VectorNorm(x);
+                x = (1 / norma) * x;
+                x.Print();
+            }
+
+            else if (choice == 2) {
+                double sigma;
+                cout << "Введите начальное приближение sigma: ";
+                cin >> sigma;
+
+                Vector x(n);
+                vector<double> init(n, 1.0);
+                x.numbersVector = init;
+
+                inversePowerMethod(sigma, x, A, n, tol);
+
+                cout << "Найденное eigenvalue: " << sigma << endl;
+                cout << "Собственный вектор:\n";
+                double norma = L2VectorNorm(x);
+                x = (1 / norma) * x;
+                x.Print();
+            }
+
+            else if (choice == 3) {
+                vector<double> eigenvalues = QR_algorithm(A, n, tol);
+
+                cout << "Собственные значения:\n";
+                for (double val : eigenvalues)
+                    cout << val << " ";
+                cout << endl;
+            }
+
+            else {
+                cout << "Неверный выбор\n";
+            }
+        }
+    }
+
+    return 0;
+}
